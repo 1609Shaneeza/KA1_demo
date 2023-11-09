68ea4dc3d61e986c8f7694117ebe80d9bd74ec99
Shaneeza <S299543@student.salfordcc.ac.uk>
2023-11-09 13:42:17 +0000

    stock searcher

--- src/StockSearcher.tsx
+++ src/StockSearcher.tsx
@@ -1,18 +1,37 @@
-import { FormEvent } from "react"
+import axios from "axios"
+import { FormEvent, useState } from "react"
+
 
 function StockSearcher() {
+    const [stockcode, setStockCode] = useState("");
     const handleClick = async(e: FormEvent) => {
         e.preventDefault();
 
-    }
+        const params = {
+            function: "TIME_SERIES_INTRADAY",
+            symbol: stockcode,
+            interval: "5min",
+            apiKey:"UK6ZNP3MKPAOFNZG"
+        };
+
+        const response = await axios.get("", {
+            params: alphaVantageParams     
+        });
+    };
 
     return(
         <>
         <form onSubmit={handleClick}>
             <label htmlFor="stockcode">Stock Code</label>
-            <input type="text" autoComplete="off" />
+            <input type="text"
+            id = "StockcodeTextbox" 
+            autoComplete="off" 
+            onChange={e => setStockCode(e.target.value)} 
+            value={stockcode} />
             <button>Submit</button>
         </form>
         </>
     )
 }
+
+export default StockSearcher;
