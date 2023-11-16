8b34026041ef2d771c7e36decc3573c900690ef6
Shaneeza <S299543@student.salfordcc.ac.uk>
2023-11-16 13:24:08 +0000

    16/11/2023

---
+++ src/NameProvider.tsx
@@ -0,0 +1,18 @@
+import { ReactNode, createContext, useState } from "react";
+
+interface NameContextType {
+    name: string,
+    setName: (name:string) => void;
+}
+const NameContext = createContext<NameContextType | null>(null);
+
+function NameProvider({ children }: {children: ReactNode}){
+    const [name, setName] = useState("");
+
+    return(
+        <NameContext.Provider value={{name, setName }}>
+            {children}
+        </NameContext.Provider>
+    )
+
+}
