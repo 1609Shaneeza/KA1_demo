79fbe90d440f127c8135c65a64c189d8e39e7905
Shaneeza <S299543@student.salfordcc.ac.uk>
2023-11-22 13:48:43 +0000

    22/11/2023

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import UserDataDisplay from "./UserDataDisplay";
 import UserDataProvider from "./UserDataProvider";
 import UserDataForm from "./UserDataForm";
 
 function App() {
 return(
     <>
+    <div className="box">
     <h2>Hello World</h2>
     <UserDataProvider>
         <div className="bordered">
             <UserDataForm/>
         </div>
     </UserDataProvider>
+    </div>
 
     </>
 );
 
 }
 
-export default App
+export default App;
