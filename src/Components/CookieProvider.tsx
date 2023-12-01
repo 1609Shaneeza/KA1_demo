ee9740fb258fa9bbafce8648566389b16b06af41
1609Shaneeza <144433002+1609Shaneeza@users.noreply.github.com>
2023-12-01 00:31:52 +0000

    ka2

--- src/Components/CookieProvider.tsx
+++ src/Components/CookieProvider.tsx
@@ -11,11 +11,35 @@ export const cookieContext = createContext < CookieContext | null>(null)
 
 function CookieProvider({children}: {children: ReactNode}) {
     const [cookieCounter, setCookieCounter] = useState(0);
+    const [cookieMultiplier, setCookieMultiplier] = useState(1);
+    
+    const handleClick = () => {
+        setCookieCounter(0);
+        setCookieCounter(cookieCounter + 1)
+    }
+    
+    const cookiesMultiplier =() => {
+        setCookieMultiplier(1)
+        
+    }
+    
+
+
+    const multiplier = () => {
+        if (cookieCounter => 100){
+            setCookieMultiplier()
+            setCookieMultiplier(cookieMultiplier + 1)
+        }
+    }
+
 
 
 
     return (
         <>
+        <cookieContext.Provider value ={{cookieCounter, setCookieCounter}}>
+            {children}
+        </cookieContext.Provider>
 
         </>
     );
