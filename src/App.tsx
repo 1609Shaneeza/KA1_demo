06b1ff6aaab9b3c96d51d1dfacffe584416c739e
1609Shaneeza <144433002+1609Shaneeza@users.noreply.github.com>
2023-12-01 09:02:53 +0000

    30/11/2023

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import CookieClicker from "./Components/CookieClicker";
 import Homepage from "./Components/HomePage";
 import Temperature from "./Components/TempChecker";
